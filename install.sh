54e199357c2dbd217de5c40e8ca292849023ddec
Jashaswimalya Acharjee <31704400+jash-maester@users.noreply.github.com>
2023-05-06 12:43:41 +0530

    Changed picom fork to FT-Labs' Fork

--- install.sh
+++ install.sh
@@ -48,8 +48,13 @@ cd ~/clipmenu && sudo make install
 
 # Install Jonaburg's Fork of Picom
 #sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev ninja meson asciidoc
+#cd ~/
+#git clone https://github.com/jonaburg/picom/
+#cd ~/picom && meson --buildtype=release . build && ninja -C build && sudo ninja -C build install
+
+# FT-Labs Picom Fork
 cd ~/
-git clone https://github.com/jonaburg/picom/
+git clone https://github.com/FT-Labs/picom.git
 cd ~/picom && meson --buildtype=release . build && ninja -C build && sudo ninja -C build install
 
 # Install skippy-xd
